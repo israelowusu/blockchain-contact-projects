ddddc82d65e80bcedbd6448343bd147c6ff2a59a
israelowusu <oaisrael633@gmail.com>
2024-08-07 17:16:40 +0100

    first contract

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.18; // this is the solidity version
+
+contract SimpleStorage {
+    // Basic Types: boolean, uint, int, address, bytes
+    uint256 myFavoriteNumber; // favoriteNumber gets initialized to 0 if no value is given
+
+    // uint256[] listofFavoriteNumbers;
+
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // dynamic array
+    Person[] public listofPeople; //[]
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    // view, pure
+    function retrieve() public view returns(uint256){
+        return myFavoriteNumber;
+    }
+
+    // calldata, memory, storage
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listofPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
